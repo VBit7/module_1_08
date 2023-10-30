533af4b2877cf2994ab1bfa4e745327118513e61
Vitalii Bielimov <vbit2000@gmail.com>
2023-10-30 08:27:28 +0200

    added ex_01

---
+++ ex_01.py
@@ -0,0 +1,26 @@
+"""
+Розробіть функцію get_days_from_today(date), яка повертатиме кількість днів від поточної дати,
+де параметр date - це рядок формату '2020-10-09' (рік-місяць-день).
+
+Підказки:
+
+Параметр date розбити на рік, місяць та день можна використовуючи метод рядків split.
+datetime приймає аргументи типу int, використовуйте перетворення типів.
+ігноруйте години, хвилини та секунди для вашої дати, важливі повні дні.
+кількість днів ви можете отримати відніманням з поточної дати, заданої в змінній date (без часу).
+Наприклад: Якщо поточна дата - '5 травня 2021', то виклик get_days_from_today("2021-10-09") поверне нам -157.
+"""
+
+from datetime import datetime
+
+
+def get_days_from_today(date):
+    date_list = date.split('-')
+    date_in = datetime(year=int(date_list[0]), month=int(date_list[1]), day=int(date_list[2]))
+    result = (datetime.now().date() - date_in.date()).days
+
+    return result
+
+
+print(get_days_from_today('2020-10-09'))
+    
